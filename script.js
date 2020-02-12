04705c509350f8a386a1caa559cf5e7c5b6521a0
fabioaraujo76 <52252478+fabioaraujo76@users.noreply.github.com>
2020-02-12 23:21:04 +0000

    Updated Navbar and sections About and Services

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //JQuery smooth scrolling
 
-$('.navbar a').on('click', function (e) {
-    if (this.hash !== '') {
-        e.preventDefault();
-        const hash = this.hash;
+// $('#navbar-slowS a').on('click', function (e) {
+//     if (this.hash !== '') {
+//         e.preventDefault();
+//         const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800);
-    }
-});
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//     }
+// });
